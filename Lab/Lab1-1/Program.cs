4d26035488690f646ea042ad4cc818f9d5d21adb
naret thummati <naraedza@gmail.com>
2015-08-21 16:10:21 +0700

    larger to smaller

--- Lab/Lab1-1/Program.cs
+++ Lab/Lab1-1/Program.cs
@@ -11,9 +11,10 @@ namespace Lab1_1
         static void Main(string[] args)
         {
             bool flag = true;
-
+            //int ii = 0;
             // input
             int[] input = { 4, 5, 2, 8, 9, 1, 2, 4, 3, 1 };
+           
 
             // process
             while (flag)
@@ -21,12 +22,13 @@ namespace Lab1_1
                 flag = false;
                 for (int i = 0; i != input.Length - 1; i++)
                 {
-                    if (input[i] > input[i + 1])
+                    if (input[i] < input[i + 1])
                     {
                         int temp = input[i];
                         input[i] = input[i + 1];
                         input[i + 1] = temp;
                         flag = true;
+                        //ii++;
                     }
                 }
             }
@@ -37,6 +39,8 @@ namespace Lab1_1
                 Console.Write(input[i]);
                 Console.Write(" ");
             }
+
+            //Console.Write(ii);
             Console.ReadKey();
         }
     }
